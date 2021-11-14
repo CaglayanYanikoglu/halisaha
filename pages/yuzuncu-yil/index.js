87084f5894c47941a1856dd625ce824acd831305
Atakan Demircioğlu <31544037+atakde@users.noreply.github.com>
2021-11-14 21:53:56 +0300

    index :: pass props

--- pages/yuzuncu-yil/index.js
+++ pages/yuzuncu-yil/index.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import SoccerMain from '../../components/SoccerMain';
+import React from "react";
+import SoccerMain from "../../components/SoccerMain";
 
-export default function YuzuncuYilSoccer() {
+const YuzuncuYilSoccer = (props) => {
+  console.log(props);
   return (
     <>
-      <SoccerMain />
+      <SoccerMain {...props} />
     </>
   );
 };
+
+export const getServerSideProps = async () => {
+  const response = await fetch(process.env.API_URL + "feed?type=NORMAL");
+  const { matches, players } = await response.json();
+  return {
+    props: { matches, players },
+  };
+};
+
+export default YuzuncuYilSoccer;
