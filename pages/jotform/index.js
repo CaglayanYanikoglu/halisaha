7151215f3c5bb42df61287cf13dc3c537789539a
Atakan Demircioğlu <31544037+atakde@users.noreply.github.com>
2021-11-14 14:13:37 +0300

    Heroku settings (#1)
    * package :: update port
    
    * yarn :: remove lock file
    
    * Procfile :: init
    
    * index :: update url
    
    * gitignore :: local env

--- pages/jotform/index.js
+++ pages/jotform/index.js
@@ -11,7 +11,7 @@ const JotformSoccer = (props) => {
 };
 
 export const getServerSideProps = async () => {
-  const response = await fetch("http://localhost:3000/api/feed?type=JF");
+  const response = await fetch(process.env.API_URL + "feed?type=JF");
   const { matches, players } = await response.json();
   return {
     props: { matches, players },
